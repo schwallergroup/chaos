7f7bedac06c4a4154ac2002eef9c70a2bef3cb51
Bojana <bojana.nu@gmail.com>
2023-08-14 16:51:51 +0200

    added training script

--- train_cli.py
+++ train_cli.py
@@ -1,11 +1,5 @@
 import argparse
 import logging
-import os
-import random
-import string
-import sys
-
-import gpytorch
 from botorch.models.model import Model as BaseModel
 from pytorch_lightning.callbacks import Timer
 from pytorch_lightning.cli import (
@@ -17,14 +11,17 @@ from pytorch_lightning.loggers import WandbLogger
 
 import wandb
 from chaos.bo.module import BoModule
-from chaos.data.module import BOAdditivesDataModule
+from chaos.data.module import BOAdditivesDataModule, BaseDataModule
+from chaos.surrogate_models.gp import SimpleGP, GP
 from chaos.initialization.initializers import BOInitializer
 
-# from chaos.surrogate_models.gp import GP  # noqa F401
 from chaos.utils import flatten
+from chaos.utils import convert_to_nested_dict
 
 logging.getLogger("PIL").setLevel(logging.WARNING)
 logging.getLogger("PIL.PngImagePlugin").setLevel(logging.CRITICAL + 1)
+
+import copy
 
 
 def get_mol_or_rxn_smile(representation):
@@ -43,29 +40,46 @@ def get_distance_metric(kernel):
     return "euclidean"
 
 
+from argparse import Namespace
+
+
+def convert_to_dict(ns):
+    result = {"class_path": ns.class_path, "init_args": {}}
+    for key, value in vars(ns).items():
+        if key == "class_path":
+            continue
+
+        keys = key.split(".")
+        d = result["init_args"]
+        for k in keys[:-1]:
+            d = d.setdefault(k, {})
+        d[keys[-1]] = value
+    return result
+
+
 class MyLightningCli(LightningCLI):
     def add_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
         parser.add_argument("--n_iters", type=int, default=100)
         parser.add_argument("--n_trials", type=int, default=20)
 
         parser.add_class_arguments(BOInitializer, "data_selection")
-        parser.add_subclass_arguments(BaseModel, "surrogate_model")
-        parser.add_subclass_arguments(gpytorch.kernels.Kernel, "kernel")
+        # parser.add_subclass_arguments(BaseModel, "surrogate_model")
+        # parser.add_subclass_arguments(BaseModel, "surrogate_model")
+        parser.add_subclass_arguments(SimpleGP, "surrogate_model")
+        # parser.add_subclass_arguments(GP, "surrogate_model")
+
+        # parser.link_arguments(
+        #     "data.representation",
+        #     "data.featurize_column",
+        #     apply_on="parse",
+        #     compute_fn=get_mol_or_rxn_smile,
+        # )
+        parser.link_arguments(
+            "data_selection", "data.initializer", apply_on="instantiate"
+        )
 
         parser.link_arguments(
-            "data.representation",
-            "data.featurize_column",
-            apply_on="parse",
-            compute_fn=get_mol_or_rxn_smile,
-        )
-        parser.link_arguments(
-            "data_selection", "data.init_selection_method", apply_on="instantiate"
-        )
-        parser.link_arguments(
             "data_selection.n_clusters", "data.init_sample_size", apply_on="instantiate"
-        )
-        parser.link_arguments(
-            "kernel", "surrogate_model.init_args.kernel", apply_on="instantiate"
         )
 
         parser.link_arguments(
@@ -74,15 +88,23 @@ class MyLightningCli(LightningCLI):
         parser.link_arguments(
             "data.train_y", "surrogate_model.init_args.train_y", apply_on="instantiate"
         )
-        # parser.link_arguments(
-        #     "data.noise", "surrogate_model.init_args.noise_val", apply_on="instantiate"
-        # )
         parser.link_arguments("data", "model.data", apply_on="instantiate")
-
-        parser.link_arguments("surrogate_model", "model.model", apply_on="instantiate")
         parser.link_arguments("n_iters", "trainer.max_epochs")
 
         return super().add_arguments_to_parser(parser)
+
+    def instantiate_classes(self) -> None:
+        """Instantiates the classes and sets their attributes."""
+        model_config = argparse.Namespace(**self.config["surrogate_model"])
+        model_config_dict = convert_to_nested_dict(vars(model_config))
+        # model_config_dict = convert_to_dict(model_config)  # vars(model_config)
+        self.config.model.model_config = model_config_dict
+        self.config_init = self.parser.instantiate_classes(self.config)
+        # self.config_init.model.model_config = model_config_dict_copy
+        self.datamodule = self._get(self.config_init, "data")
+        self.model = self._get(self.config_init, "model")
+        self._add_configure_optimizers_method_to_model(self.subcommand)
+        self.trainer = self.instantiate_trainer()
 
     def before_instantiate_classes(self) -> None:
         return super().before_instantiate_classes()
@@ -91,71 +113,21 @@ class MyLightningCli(LightningCLI):
 class WandbSaveConfigCallback(SaveConfigCallback):
     def __init__(self, parser, config, overwrite=True):
         super().__init__(parser, config, overwrite=overwrite)
-        print(self.config)
         self.parser = parser
         self.config = config
         wandb_config = flatten(dict(self.config))
         wandb.config.update(wandb_config)
 
 
-def reset_wandb_env():
-    exclude = {
-        "WANDB_PROJECT",
-        "WANDB_ENTITY",
-        "WANDB_API_KEY",
-    }
-    for k, v in os.environ.items():
-        if k.startswith("WANDB_") and k not in exclude:
-            del os.environ[k]
-
-
-letters = string.ascii_lowercase
-
-
-def generate_group_name():
-    return "".join(random.choice(letters) for i in range(10))
-
-
-def start_new_run(seed):
-    print(sys.argv)
-    cli = MyLightningCli(
-        model_class=BoModule,
-        datamodule_class=BOAdditivesDataModule,
-        run=False,
-        save_config_callback=WandbSaveConfigCallback,
-        save_config_kwargs={"overwrite": True},
-        trainer_defaults={
-            "logger": WandbLogger(  # save_dir=f'./wandb-save-dir/{group}',
-                project="additives-debugging"
-            ),  # , reinit=True),
-            "log_every_n_steps": 1,
-            "min_epochs": 1,
-            "max_steps": -1,
-            "accelerator": "cpu",
-            "devices": 1,
-            # "reload_dataloaders_every_n_epochs": 1,
-            "num_sanity_val_steps": 0,
-            # "callbacks": [Timer()],
-        },
-        args=sys.argv.append(f"--seed={seed}"),
-        # save_config_overwrite=True,
-        # seed_everything_default=seed
-    )
-    return cli
-
-
 def cli_main():
-    group = generate_group_name()
     cli = MyLightningCli(
         model_class=BoModule,
-        datamodule_class=BOAdditivesDataModule,
+        datamodule_class=BaseDataModule,
         run=False,
         save_config_callback=WandbSaveConfigCallback,
         save_config_kwargs={"overwrite": True},
         trainer_defaults={
-            "logger": WandbLogger(  # save_dir=f'./wandb-save-dir/{group}',
-                project="additives-report"
-            ),
+            "logger": WandbLogger(project="additives-rebuttal"),
             "log_every_n_steps": 1,
             "min_epochs": 1,
             "max_steps": -1,
@@ -166,6 +138,7 @@ def cli_main():
     )
 
     cli.trainer.fit(cli.model)
+    # wandb.save('config.yaml')
     wandb.finish()
 
 
